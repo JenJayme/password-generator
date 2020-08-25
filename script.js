4b0c91da8b3a2fa13f8e22396fba2dcb409cbc1e
Jen Jayme <jeniferjayme@gmail.com>
2020-08-24 20:22:39 -0700

    Added prompts to get user preferences.

--- script.js
+++ script.js
@@ -28,58 +28,90 @@ function generatePassword () {
     var newPassword = "";
 
     //variable to hold user's chosen length
-    var chosenLength = 8;
+    var chosenLength = chosenLength = prompt ("How many characters would you like your password to have?  Please choose a number between 8 and 128.");
 
     //function to push selected character types into characterPool array
   function createCharacterPool () {
 
     alert("Creating character pool...");
 
-    var lowercase = "abcdefghijklmnopqrstuv";
+    //pool of types to include
+    var includedTypes = [];
+    var characterPool = [];
+
+    //pool of characters from which to choose after user specifies their desired types
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbers = "0123456789";
     var specialChar = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
     // var specialArr = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "/", ",", ":", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"]  ]
 
-    //pool of types to include
-    var types = ["lowercase", "numbers"];
+    var hasLowercase, hasUppercase, hasNumbers, hasSpecial;
 
-    //pool of characters from which to choose after user specifies their desired types
-    var characterPool = [];
+      if (confirm ("Would you like to include LOWERCASE LETTERS?") == true) { 
+        hasLowercase = true;
+        includedTypes.push("Lowercase");
+        characterPool.concat(lowercase);
+      }
 
-    //variable to hold user's chosen length
-    var chosenLength = 8;
+      if (confirm ("Would you like to include UPPERCASE LETTERS?") == true) {
+        hasUppercase = true; 
+        includedTypes.push("Uppercase");
+        characterPool.concat(uppercase);
+      }
+
+      if (confirm ("Would you like to include NUMBERS?") == true) {
+        hasNumbers = true;
+        includedTypes.push("Numbers");
+        characterPool.concat(numbers);
+      }
+
+      if (confirm ("Would you like to include SPECIAL CHARACTERS?") == true) { 
+        hasSpecial = true;
+        includedTypes.push("Special");
+        characterPool.concat(specialChar);
+      }
+
+      if (chosenLength <8 || chosenLength > 128) { alert ("Oops! That's not a number between 8 and 128. Please choose again.") }
+
+      console.log("characterPool =", characterPool)
+}
 
 
-    switch(types) {
-        case includes("lowercase"):
-        characterPool.push(lowercase);
-        console.log("with lowercase", characterPool)
-        break;
+    //==========================================================================
 
-        case includes("uppercase"):
-            characterPool.push(uppercase);
-            console.log("with uppercase", characterPool)
-        break;
+    // switch(types) {
+    //     case types.includes("lowercase"):
+    //     characterPool.push(...lowercase);
+    //     console.log("with lowercase", characterPool)
+    //     break;
 
-        case includes("numbers"):
-            characterPool.push(numbers);
-            console.log("with numbers", characterPool)
-        break;
+    //     case types.includes("uppercase"):
+    //         characterPool.push(uppercase);
+    //         console.log("with uppercase", characterPool)
+    //     break;
 
-        case includes("uppercase"):
-            characterPool.push(specialChar);
-            console.log("with special", characterPool)
-        break;
+    //     case types.includes("numbers"):
+    //         characterPool.push(numbers);
+    //         console.log("with numbers", characterPool)
+    //     break;
 
-        default:
-            console.log("Other type found", types[0]);
-            console.log("Character Pool default", characterPool)
-        return types;
-    }
+    //     case types.includes("uppercase"):
+    //         characterPool.push(specialChar);
+    //         console.log("with special", characterPool)
+    //     break;
 
-      console.log("Character pool at end", characterPool)
-  }
+    //     default:
+    //         console.log("Running default in switch function.");
+    //         console.log("Character Pool default", characterPool)
+    //     return types;
+    // }
+
+    //   console.log("Types after switch function", types);
+    //   console.log("Character pool at end", characterPool)
+
+  
+    //==========================================================================
 
   function generateRandomItem () {
         //variable to temporarily hold next character before concat  
